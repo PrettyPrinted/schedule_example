1cef887e3da1cfd66ebb51c4d747906c3b855bae
Anthony Herbert <anthony@prettyprinted.com>
2023-08-22 11:38:22 +0700

    added volume for redis

--- project/views.py
+++ project/views.py
@@ -8,7 +8,7 @@ main = Blueprint("main", __name__)
 @main.route("/webhook")
 def webhook():
     schedule_name = "test_schedule" # can be whatever you want but should be unique for each scheduled task
-    interval = crontab(month_of_year=8, day_of_month=22, hour=4, minute=30)
+    interval = crontab(month_of_year=8, day_of_month=22, hour=4, minute=40)
     entry = RedBeatSchedulerEntry(schedule_name, 'project.tasks.withdrawal', interval, args=[12345, schedule_name], app=celery_app)
     entry.save()
 
